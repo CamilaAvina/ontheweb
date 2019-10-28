818d9208a8505cfbc893bc186cad5413b1b1ae07
Camila Avina <twin2avina@gmail.com>
2019-10-27 18:57:21 -0700

    trying to get the code to work

--- sketch.js
+++ sketch.js
@@ -1,10 +1,57 @@
+//let button;
+// let slider;
+//let input;
+
+
 
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
    background(0);
+   createButtom("click me");
+  //slider= createSlider(10,100,86);
+
+
+
+  //nameInput.parent("inputFields");
+  // button = select('#Button');
+  // button.mousePressed(buttonPressed);
+  // button.class("Btn")
+  // slider = createSlider(0, 255, 100);
+  // slider.position(200, 110);
+  // slider.style('width', '80px');
 }
 
 function draw() {
+  // val = slider.value();
+  // background(val);
 
 }
+
+
+
+
+
+
+
+
+
+// let checkboxes = [];
+// let size=100;
+// function setup() {
+//   createCanvas(500, 500);
+//    background(0);
+//    for (let i = 0; i <100; i ++ ){
+//   checkboxes[i] = createCheckbox('label');
+//
+//   checkboxes[i].style('display','inline');
+//   checkboxes[i].position(random(windowWidth)),(random(windowHeight));
+//   checkboxes[i].changed(myCheckedEvent);
+// }
+// }
+// function myCheckedEvent();
+//
+// function draw() {
+//   ellispe(mouseX,mouseY,size);
+//
+// }
