9250d6b6b456ffc69fec959da07bdf79cf5f1be7
Camila Avina <twin2avina@gmail.com>
2019-10-27 21:19:52 -0700

    trying to see what is going on

--- sketch.js
+++ sketch.js
@@ -1,35 +1,31 @@
-//let button;
-// let slider;
-//let input;
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//    background(0);
+// }
+//
+// function draw() {
+//
+// }
 
 
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-   background(0);
-   createButtom("click me");
-  //slider= createSlider(10,100,86);
 
 
 
-  //nameInput.parent("inputFields");
-  // button = select('#Button');
-  // button.mousePressed(buttonPressed);
-  // button.class("Btn")
-  // slider = createSlider(0, 255, 100);
-  // slider.position(200, 110);
-  // slider.style('width', '80px');
-}
 
-function draw() {
-  // val = slider.value();
-  // background(val);
+
+
+function setup(){
+createCanvas(windowWidth,windowHeight);
+background(0)
 
 }
 
+function draw(){
+  ellispe(mouseX,mouseY,size);
 
-
+}
 
 
 
